04372ac77e12b7b2cb4b4f4ef4969d3b95838998
Flavio De Souza <fdesouzabcn@gmail.com>
2026-02-19 21:04:45 +0100

    feat: install and configure laravel passport

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [                      // ← Add this block
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
     ],
 
     /*
